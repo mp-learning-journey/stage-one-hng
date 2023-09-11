fc921d4abeda2fd30a5707d29b719ea83e41fb3d
mrprotocoll <lekanvgbg@gmail.com>
2023-09-11 10:28:39 +0100

    update api.php

--- api.php
+++ api.php
@@ -1,38 +1,31 @@
 <?php
-header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers");
 
-$request_method = $_SERVER["REQUEST_METHOD"];
+$slack = $_GET['slack_name'] ?? "";
+$track = $_GET['track'] ?? "";
 
-$slack = $_GET['slack_name'];
-$track = $_GET['track'];
+if($_SERVER["REQUEST_METHOD"] == 'GET') {
+    if(empty($slack) || empty($track)) {
+        http_response_code(422);
+        echo json_encode(["error" => 'slack_name and track are required']);
+    }else{
+        $response = [
+            "slack_name" => $slack,
+            "current_day" => date('D'),
+            "utc_time" => gmdate('Y-m-d\TH:i:s\Z'),
+            "track" => $track,
+            "github_file_url" => "https://github.com/mp-learning-journey/stage-one-hng/blob/main/api.php",
+            "github_repo_url" => "https://github.com/mp-learning-journey/stage-one-hng",
+            "status_code" => 200
+        ];
 
-if(empty($slack) || empty($track)) {
-    json_encode(["error" => 'slack_name and track are required']);
-}
-
-$response = [
-    "slack_name" => $slack,
-    "current_day" => date('D'),
-    "utc_time" => gmdate('Y-m-d\TH:i:s\Z'),
-    "track" => $track,
-    "github_file_url" => "https://github.com/username/repo/blob/main/file_name.ext",
-    "github_repo_url" => "https://github.com/mp-learning-journey/stage-one-hng",
-    "status_code" => 200
-];
-
-switch ($request_method) {
-    case 'GET':
-        // Handle GET request (Read)
-        if (!empty($_GET["id"])) {
-            $id = intval($_GET["id"]);
-            get_book($id);
-        } else {
-            get_books();
-        }
-        break;
-    default:
-        // Invalid request method
-        header("HTTP/1.0 405 Method Not Allowed");
-        json_encode(["message" => "Invalid request"], 405);
-        break;
+        http_response_code(200);
+        echo json_encode($response);
+    }
+}else{
+    header("HTTP/1.1 404 Route Not Found");
+    echo json_encode(["error" => "Route not found"]);
 }
